a9ca21990ef0094e3668aa05f372423d989089e7
Laurent Hasson <ldhasson@users.noreply.github.com>
2017-12-02 23:59:32 -0500

    - added a self-signed cert to make things easier - removed dependency on env variables.

--- src/tildabuilder/Starter.java
+++ src/tildabuilder/Starter.java
@@ -12,56 +12,63 @@ import org.apache.catalina.webresources.StandardRoot;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import tilda.utils.TextUtil;
+
 
 public class Starter
   {
-    protected static final Logger LOG        = LogManager.getLogger(Starter.class.getName());
-    public static Connector getSecureConnector()
-    throws Exception
-    {
-      Connector secureConnector = new Connector();
-      String webPort = System.getenv("PORT");
-      if(webPort == null || webPort.isEmpty()) {
-          webPort = "8443";
-          LOG.info("DEFAULT: Listening on port "+webPort);
-      }
-      String keystoreFile = System.getenv("keystoreFile");
-      String keystorePass = System.getenv("keystorePass");
-      if(keystoreFile == null || keystoreFile.isEmpty() || keystorePass == null || keystorePass.isEmpty())
-        {
-          if(keystoreFile == null || keystoreFile.isEmpty())
-            {
-              LOG.error("Environment variable keystoreFile is required");
-            }
-          if(keystorePass == null || keystorePass.isEmpty())
-            {
-              LOG.error("Environment variable keystoreFile is required");
-            }
-          throw new Exception("Missing required Environmental variables");
-        }
+    protected static final Logger LOG = LogManager.getLogger(Starter.class.getName());
 
-      secureConnector.setPort(Integer.valueOf(webPort));
-      secureConnector.setSecure(true);
-      secureConnector.setScheme("https");
-      secureConnector.setProperty("SSLEnabled", "true");
-      secureConnector.setProperty("keystoreFile", keystoreFile);
-      secureConnector.setProperty("keystorePass", keystorePass);
-      secureConnector.setProperty("clientAuth", "false");
-      secureConnector.setProperty("sslProtocol", "TLS");
-      return secureConnector;
-    }
+    public static Connector getSecureConnector(String webPort, String keystoreFile, String keystorePass)
+    throws Exception
+      {
+        Connector secureConnector = new Connector();
+        if (TextUtil.isNullOrEmpty(webPort) == true)
+          webPort = "8843";
+        LOG.info("Listening on port " + webPort);
+        boolean Error = false;
+        if (TextUtil.isNullOrEmpty(keystoreFile) == true)
+          {
+            LOG.error("Environment variable keystoreFile is required");
+            Error = true;
+          }
+        if (TextUtil.isNullOrEmpty(keystorePass) == true)
+          {
+            LOG.error("Environment variable keystorePass is required");
+            Error = true;
+          }
+        if (Error == true)
+          throw new Exception("Missing required Environmental variables");
+
+        secureConnector.setPort(Integer.valueOf(webPort));
+        secureConnector.setSecure(true);
+        secureConnector.setScheme("https");
+        secureConnector.setProperty("SSLEnabled", "true");
+        secureConnector.setProperty("keystoreFile", keystoreFile);
+        secureConnector.setProperty("keystorePass", keystorePass);
+        secureConnector.setProperty("clientAuth", "false");
+        secureConnector.setProperty("sslProtocol", "TLS");
+        return secureConnector;
+
+      }
+
     public static void main(String[] args)
     throws Exception
       {
         String WebappPath = new File("WebContent").getAbsolutePath();
         Tomcat tomcat = new Tomcat();
-        tomcat.setConnector(getSecureConnector());
+
+        String webPort = System.getenv("PORT");
+        String keystoreFile = "..//tilda.bin"; // System.getenv("keystoreFile");
+        String keystorePass = "tildaxyz123"; // System.getenv("keystorePass");
+
+        tomcat.setConnector(getSecureConnector(webPort, keystoreFile, keystorePass));
         Context context = tomcat.addWebapp("/TildaBuilder", WebappPath);
         // Path to Servlet classes
         File additionWebInfClassesFolder = new File("bin");
         WebResourceRoot resources = new StandardRoot(context);
         WebResourceSet resourceSet;
-        if (additionWebInfClassesFolder.exists()) 
+        if (additionWebInfClassesFolder.exists())
           {
             resourceSet = new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClassesFolder.getAbsolutePath(), "/");
             LOG.debug("loading WEB-INF resources from as '" + additionWebInfClassesFolder.getAbsolutePath() + "'");
